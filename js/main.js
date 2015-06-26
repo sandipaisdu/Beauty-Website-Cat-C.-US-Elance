c8ed71cf8c7d1718acb0a87af17afb7ca2d0b2b0
Sandipaisdu <ksandipdu@gmail.com>
2015-06-26 06:56:23 +0600

    Finished

--- js/main.js
+++ js/main.js
@@ -1,22 +1,2 @@
-    // set everything outside the onscroll event (less work per scroll)
-var left      = document.getElementById("contact"),
-    // -60 so it won't be jumpy
-    stop      = left.offsetTop - 60,
-    docBody   = document.documentElement || document.body.parentNode || document.body,
-    hasOffset = window.pageYOffset !== undefined,
-    scrollTop;
-
-window.onscroll = function (e) {
-  // cross-browser compatible scrollTop.
-  scrollTop = hasOffset ? window.pageYOffset : docBody.scrollTop;
-
-  // if user scrolls to 60px from the top of the left div
-  if (scrollTop >= stop) {
-    // stick the div
-    left.className = 'stick';
-  } else {
-    // release the div
-    left.className = ''; 
-  }
-}
+$('.container').stickem();
 
